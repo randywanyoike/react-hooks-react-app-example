cbcddacd8e5212242a7ca20c85bc20b80b5350f8
randywanyoike <randywanyoike@gmail.com>
2025-07-23 10:46:30 +0300

    Complete lab

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ExampleComponent from "./ExampleComponent";
+import TestComponent from "./TestComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>Now</h1>
+      <ExampleComponent />
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TestComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function TestComponent() {
+  return (
+    <video title="time video" />
+  );
+}
